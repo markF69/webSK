9e7a23de1d530c6f05b7085c293b23c42d4c5d86
markF69 <69148528+markF69@users.noreply.github.com>
2020-09-16 10:36:33 +0200

    Update operatori.php

--- operatori.php
+++ operatori.php
@@ -5,3 +5,51 @@
 
     echo $b;
     echo $b;
+
+    echo "<hr>";
+    
+    $a = true;
+    $b = false;
+    $c = $a || $b;
+    $d = $a && $b;
+    $f = (!$a && $b) || ($a && !$b);
+    $g = !$a;
+
+    echo " a = ", $a, "<br />";
+    echo " b = ", $b, "<br />";
+    echo " c = ", $c , "<br />";
+    echo " d = ", $d, "<br />";
+    echo " f = ", $f, "<br />";
+    echo " g = ", $g;
+
+    var_dump($f);
+
+    //operatori poredjenja
+    echo "<h1>Neki kurac</h1>";
+    $a = 1;
+    $b = 1;
+    $c = $a > $b;
+    var_dump($c);
+
+    if($a != $b){
+        echo "T";
+    }
+    else
+    {
+        echo "N";
+    }
+    //ternarni operator ?
+
+    ($a == $b)? $c = 10:$c = 0;
+    echo "<br>$c";
+
+    $a = 0;
+    $b = 1;
+    $c = $a??$b;
+//referentni operator dodele i necega
+    $b = 10;
+    $a = &$b;
+
+    echo "<br> a=$a b=$b";
+    $b = "string";
+    echo "<br>a=$a b=$b";
