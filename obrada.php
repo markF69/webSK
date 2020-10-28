aa6121855853c8d701a7e7a46c725b4566e499f6
markF69 <69148528+markF69@users.noreply.github.com>
2020-10-28 10:34:20 +0100

    Update obrada.php

--- obrada.php
+++ obrada.php
@@ -1,31 +1,27 @@
 <?php
 
 echo $_SERVER['PHP_SELF'];
-echo'<br>';
-//provera tipa zahteva od klijentske strane
+echo "<br>";
+
+// provera tipa zahteva od klijentske strane
 if($_SERVER['REQUEST_METHOD']=="GET"){
-    echo "Podaci nece biti obradjeni zbog pogresne metode";
+    echo "Podaci nece biti obradjeni, zbog pogresnog metoda";
     die("Greska!!!");
 }
 
 
-
 //$podatak = $_POST['nUnos'];
 
-if(!empty($_POST['nUnos']) || strlen($_POST['nUnos']>0)) // da nije prazno i da moze da se unese nula
-{
-    echo "OK";
+if(!empty($_POST['nUnos']) || strlen($_POST['nUnos'])>0){ // da nije prazno i da moze da se unese 0
+    echo "OK!!";
     $podatak = $_POST['nUnos'];
+    var_dump($podatak);
+}else{
+    echo "Greska: podatak ne postoji!!!";
 }
-else
-{
-    echo "Greska podatak ne postoji";
-}
 
 
-
-//$_REQUEST - preuzimanje podataka (zahteva) sa klijentske strane
-
+// $_REQUEST - preuzimanje podataka (zahteva) sa klijentske strane
 $unos = $_REQUEST['nUnos'];
 
 echo "<hr>";
